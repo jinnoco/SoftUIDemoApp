c6d6a0fa560793190d1dadcf30c3494c2f105af9
nofprog <k.jin3217@gmail.com>
2021-06-13 14:04:58 +0900

    SoftUI

--- NeumorphismDemoApp/ViewController.swift
+++ NeumorphismDemoApp/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import SoftUIView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor(red: 238/255, green: 238/255, blue: 238/255, alpha: 1)
+        
+        let softUIView = SoftUIView(frame: .init(x: 100, y: 300, width: 200, height: 200))
+        softUIView.cornerRadius = 100.0
+        view.addSubview(softUIView)
     }
 
+    
+    
 
 }
 
